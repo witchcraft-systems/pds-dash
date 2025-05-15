0a6279823efa8ca3c323b07f031791561454e838
Ari <ariadna@hey.com>
2025-05-15 01:36:19 -0400

    Build step for the css themes continued

---
+++ theming.ts
@@ -0,0 +1,30 @@
+import { Plugin } from 'vite';
+import { Config } from './config';
+// Replaces app.css with the contents of the file specified in the
+// config file.
+
+export const themePlugin = (): Plugin => {
+    const themeFile = Config.THEME;
+    console.log(`Using theme file: ${themeFile}`);
+    return {
+        name: 'theme-generator',
+        transform(code, id) {
+            if (id.endsWith('app.css')) {
+                // Read the theme file and replace the contents of app.css with it
+                // Needs full path to the file
+                const themeCode = Deno.readTextFileSync(Deno.cwd() + '/src/themes/' + themeFile);
+                // Replace the contents of app.css with the theme code
+
+                // and add a comment at the top
+                const themeComment = `/* Generated from ${themeFile} */\n`;
+                const themeCodeWithComment = themeComment + themeCode;
+                // Return the theme code as the new contents of app.css
+                return {
+                    code: themeCodeWithComment,
+                    map: null,
+                };
+            }
+            return null;
+        }
+    };
+};
