e49ce63db96b23bc130417a2b1cf4eb895f41a72
Ari <ariadna@hey.com>
2025-05-15 01:35:50 -0400

    Build step for the theme file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { themePlugin } from "./theming";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    themePlugin(),
+    svelte(),
+  ],
 });
