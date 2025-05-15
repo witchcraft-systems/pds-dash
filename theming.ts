fc8d61e1ef551fbd6171b4764f8bc61c9bf647a7
Astra <me@astrra.space>
2025-05-15 16:20:16 +0900

    Theming 101

--- theming.ts
+++ theming.ts
@@ -5,20 +5,20 @@ import { Config } from './config';
 // Replaces app.css with the contents of the file specified in the
 // config file.
 export const themePlugin = (): Plugin => {
-    const themeFile = Config.THEME;
-    console.log(`Using theme file: ${themeFile}`);
+    const themeFolder = Config.THEME;
+    console.log(`Using theme folder: ${themeFolder}`);
     return {
         name: 'theme-generator',
         transform(code, id) {
             if (id.endsWith('app.css')) {
-                const colorsCode = Deno.readTextFileSync(Deno.cwd() + '/src/themes/colors.css');
+                const colorsCode = Deno.readTextFileSync(Deno.cwd() + '/themes/colors.css');
                 // Read the theme file and replace the contents of app.css with it
                 // Needs full path to the file
-                const themeCode = Deno.readTextFileSync(Deno.cwd() + '/src/themes/' + themeFile);
+                const themeCode = Deno.readTextFileSync(Deno.cwd() + '/themes/' + themeFolder + '/theme.css');
                 // Replace the contents of app.css with the theme code
 
                 // and add a comment at the top
-                const themeComment = `/* Generated from ${themeFile} */\n`;
+                const themeComment = `/* Generated from ${themeFolder} */\n`;
                 const themeCodeWithComment = themeComment + colorsCode + themeCode;
                 // Return the theme code as the new contents of app.css
                 return {
